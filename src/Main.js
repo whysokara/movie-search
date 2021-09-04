ad12086dfa8a1961c7abc964ea00c2c0366dc2ff
Kara <kara_himanshu@yahoo.com>
2021-09-04 15:58:40 +0530

    Netlify deploy

---
+++ src/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SearchMovies from './SearchMovies'
+
+const Main = () => {
+  return (
+    <div className="container">
+      <h1 className="title">Find Your Movie</h1>
+      <SearchMovies />
+    </div>
+  );
+};
+
+export default Main;
